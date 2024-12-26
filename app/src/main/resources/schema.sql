82f987023eb838eb552b67024ebe524c5b74ec70
Dani <dango41rus@yandex.ru>
2024-12-26 03:29:09 +0300

    fix: db schema fixed 3, some primitive date put

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -17,21 +17,35 @@ DROP TABLE if exists sprint_points cascade;
 DROP TABLE if exists race_points cascade;
 DROP TABLE if exists status cascade;
 
+
+CREATE TABLE status(
+    id bigserial primary key,
+    status_text CHAR(4) not null,
+    status_text_full CHAR(20) not null
+);
+
 CREATE TABLE engine(
-    engine_id SMALLINT PRIMARY KEY,
-    name CHAR(100) not null,
+    id bigserial primary key,
     manufacturer CHAR(100) not null
 );
 
+CREATE TABLE team(
+    id bigserial primary key,
+    name CHAR(100) unique not null,
+    location CHAR(100) not null,
+    since int check (since > 0),
+    till int check (till > 0)
+);
+
 CREATE TABLE car(
-    car_id SMALLINT PRIMARY KEY,
+    id bigserial primary key,
     name CHAR(100) not null,
-    team_id smallint REFERENCES team(team_id),
-    engine_id SMALLINT REFERENCES engine(engine_id)
+    team_id smallint REFERENCES team,
+    engine_id SMALLINT REFERENCES engine
 );
 
 CREATE TABLE track(
-    track_id SMALLINT PRIMARY KEY,
+    id bigserial primary key,
     country CHAR(20) not null,
     length FLOAT not null check (length > 0),
     since DATE not null,
@@ -39,36 +53,23 @@ CREATE TABLE track(
     record TIME not null
 );
 
-CREATE TABLE team(
-    team_id SMALLINT PRIMARY KEY,
-    name CHAR(100) unique not null,
-    location CHAR(100) not null,
-    since DATE not null,
-    till DATE not null
-);
-
 CREATE TABLE format(
-    format_id SMALLINT PRIMARY KEY,
+    id bigserial primary key,
     practices_quantity SMALLINT not null,
     sprint BOOLEAN not null
 );
 
 CREATE TABLE constructors_championship(
-    team_id SMALLINT REFERENCES team(team_id),
-    points SMALLINT not null
-);
-
-CREATE TABLE drivers_championships(
-    driver_id SMALLINT REFERENCES driver(driver_id),
+    id SMALLINT REFERENCES team,
     points SMALLINT not null
 );
 
 CREATE TABLE driver(
-    driver_id SMALLINT PRIMARY KEY,
+    id bigserial primary key,
     name CHAR(100) not null,
     birthday DATE not null,
     country CHAR(20) not null,
-    team_id SMALLINT REFERENCES team(team_id),
+    team_id SMALLINT REFERENCES team,
     wins SMALLINT not null,
     poles SMALLINT not null,
     championships SMALLINT not null,
@@ -76,81 +77,80 @@ CREATE TABLE driver(
     till DATE
 );
 
+CREATE TABLE drivers_championships(
+    id SMALLINT REFERENCES driver,
+    points SMALLINT not null
+);
+
 CREATE TABLE tyre(
-    tyre_id SMALLINT PRIMARY KEY,
+    id bigserial primary key,
     tyre_type CHAR(15) unique not null
 );
 
 CREATE TABLE grand_prix(
-    gp_id SMALLINT PRIMARY KEY,
+    id bigserial primary key,
     name CHAR(100) unique not null,
-    track_id SMALLINT REFERENCES track(track_id),
+    track_id SMALLINT REFERENCES track,
     date DATE not null,
-    format_id SMALLINT REFERENCES format(format_id),
+    format_id SMALLINT REFERENCES format,
     since DATE not null,
     till DATE not null
 );
 
 CREATE TABLE practice(
-    gp_id SMALLINT REFERENCES grand_prix(gp_id),
-    driver_id SMALLINT REFERENCES driver(driver_id),
+    gp_id SMALLINT REFERENCES grand_prix,
+    driver_id SMALLINT REFERENCES driver,
     place SMALLINT not null,
     best_time TIME not null,
-    tyre_id SMALLINT REFERENCES tyre(tyre_id),
+    tyre_id SMALLINT REFERENCES tyre,
     laps_completed SMALLINT not null,
-    status_id SMALLINT REFERENCES status(status_id)
+    status_id SMALLINT REFERENCES status
 );
 
 CREATE TABLE qualification(
-    gp_id SMALLINT REFERENCES grand_prix(gp_id),
-    driver_id SMALLINT REFERENCES driver(driver_id),
+    gp_id SMALLINT REFERENCES grand_prix,
+    driver_id SMALLINT REFERENCES driver,
     place SMALLINT not null,
     best_time TIME not null,
-    tyre_id SMALLINT REFERENCES tyre(tyre_id),
-    status_id SMALLINT REFERENCES status(status_id)
+    tyre_id SMALLINT REFERENCES tyre,
+    status_id SMALLINT REFERENCES status
 );
 
 CREATE TABLE sprint(
-    gp_id SMALLINT REFERENCES grand_prix(gp_id),
-    driver_id SMALLINT REFERENCES driver(driver_id),
+    gp_id SMALLINT REFERENCES grand_prix,
+    driver_id SMALLINT REFERENCES driver,
     place SMALLINT not null,
     best_time TIME not null,
-    tyre_id SMALLINT REFERENCES tyre(tyre_id),
-    status_id SMALLINT REFERENCES status(status_id),
+    tyre_id SMALLINT REFERENCES tyre,
+    status_id SMALLINT REFERENCES status,
     points SMALLINT not null
 );
 
 CREATE TABLE race(
-    gp_id SMALLINT REFERENCES grand_prix(gp_id),
-    driver_id SMALLINT REFERENCES driver(driver_id),
+    gp_id SMALLINT REFERENCES grand_prix,
+    driver_id SMALLINT REFERENCES driver,
     place SMALLINT not null,
     best_time TIME not null,
-    tyre_id SMALLINT REFERENCES tyre(tyre_id),
-    status_id SMALLINT REFERENCES status(status_id),
+    tyre_id SMALLINT REFERENCES tyre,
+    status_id SMALLINT REFERENCES status,
     points SMALLINT not null
 );
 
 CREATE TABLE scoring_system(
-    sc_system_id SMALLINT PRIMARY KEY,
+    id bigserial primary key,
     info CHAR(100) not null,
     fastest_lap SMALLINT not null
 );
 
 CREATE TABLE sprint_points(
-    sc_system_id SMALLINT REFERENCES scoring_system(sc_system_id),
+    sc_system_id SMALLINT REFERENCES scoring_system,
     place SMALLINT not null,
     points SMALLINT not null
 );
 
 CREATE TABLE race_points(
-    sc_system_id SMALLINT REFERENCES scoring_system(sc_system_id),
+    sc_system_id SMALLINT REFERENCES scoring_system,
     place SMALLINT not null,
     points SMALLINT not null
-);
-
-CREATE TABLE status(
-    status_id SMALLINT PRIMARY KEY,
-    status_text CHAR(4) not null,
-    status_text_full CHAR(20) not null
 );
 
